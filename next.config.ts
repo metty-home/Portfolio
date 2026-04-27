97fbc6d500bb248ccf72634e1ab2bff54c261a21
metty <wjddmldud13@gmail.com>
2026-04-27 23:32:09 +0900

    Add GitHub Pages deployment config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // 1. 정정 사이트 변환 (빌드 시 'out' 폴더 생성)
+  output: "export",
+
+  // 2. 이미지 최적화 비활성화
+  images: {
+    unoptimized: true,
+  },
+
+  // 3. 경로설정
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio',
 };
 
 export default nextConfig;
